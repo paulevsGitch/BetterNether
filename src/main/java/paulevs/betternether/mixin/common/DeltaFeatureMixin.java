7dbf175db4c4cb1bcb01b227fc177faf36b46073
paulevsGitch <paulevs@yandex.ru>
2020-04-16 03:08:30 +0300

    Armor stand fix, removed old mixins

--- src/main/java/paulevs/betternether/mixin/common/DeltaFeatureMixin.java
+++ src/main/java/paulevs/betternether/mixin/common/DeltaFeatureMixin.java
@@ -1,7 +1,6 @@
 package paulevs.betternether.mixin.common;
 
 import org.spongepowered.asm.mixin.Mixin;
-
 import net.minecraft.world.gen.feature.DeltaFeature;
 
 @Mixin(DeltaFeature.class)
@@ -15,5 +14,21 @@ public class DeltaFeatureMixin
 			info.setReturnValue(false);
 			info.cancel();
 		}
+	}*/
+	
+	/*@Inject(method = "method_27103", at = @At("HEAD"), cancellable = true)
+	private static void checkBlock(IWorld iWorld, BlockPos blockPos, DeltaFeatureConfig deltaFeatureConfig, CallbackInfoReturnable<Boolean> info)
+	{
+		//System.out.println(iWorld.getBlockState(blockPos));
+		//if (!isValidBlock(iWorld.getBlockState(blockPos)))
+		{
+			info.setReturnValue(false);
+			info.cancel();
+		}
+	}
+	
+	private static boolean isValidBlock(BlockState state)
+	{
+		return BlocksHelper.isNetherGround(state) || state.getBlock() == Blocks.BASALT;
 	}*/
 }
