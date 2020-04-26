f29626f0f655d1b651ace3ec8620ac3935ea7142
paulevsGitch <paulevs@yandex.ru>
2020-04-27 02:31:21 +0300

    Terrain surface generation fix

--- src/main/java/paulevs/betternether/biomes/NetherBiomeWrapper.java
+++ src/main/java/paulevs/betternether/biomes/NetherBiomeWrapper.java
@@ -5,8 +5,6 @@ import java.util.Random;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.gen.surfacebuilder.SurfaceConfig;
-import paulevs.betternether.BlocksHelper;
 
 public class NetherBiomeWrapper extends NetherBiome
 {
@@ -27,7 +25,7 @@ public class NetherBiomeWrapper extends NetherBiome
 	@Override
 	public void genSurfColumn(IWorld world, BlockPos pos, Random random)
 	{
-		SurfaceConfig config = biome.getSurfaceConfig();
-		BlocksHelper.setWithoutUpdate(world, pos, config.getTopMaterial());
+		//SurfaceConfig config = biome.getSurfaceConfig();
+		//BlocksHelper.setWithoutUpdate(world, pos, config.getTopMaterial());
 	}
 }
