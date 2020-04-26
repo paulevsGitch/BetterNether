33fb77a5d91e977390c1c7575cf3e3abe26d312a
paulevsGitch <paulevs@yandex.ru>
2020-04-27 02:54:16 +0300

    Egg plant & preset fix

--- src/main/java/paulevs/betternether/mixin/client/PresetsScreenMixin.java
+++ src/main/java/paulevs/betternether/mixin/client/PresetsScreenMixin.java
@@ -10,8 +10,9 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.gui.screen.PresetsScreen;
-import net.minecraft.client.resource.language.I18n;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.gen.chunk.FlatChunkGeneratorLayer;
@@ -21,11 +22,11 @@ import net.minecraft.world.gen.chunk.FlatChunkGeneratorLayer;
 public class PresetsScreenMixin
 {
 	@Shadow
-	private static void addPreset(String name, ItemConvertible icon, Biome biome, List<String> structures, FlatChunkGeneratorLayer... layers) {}
+	private static void addPreset(Text text, ItemConvertible icon, Biome biome, List<String> structures, FlatChunkGeneratorLayer... layers) {}
 	
 	static
 	{
-		addPreset(I18n.translate("betternether.flat_nether"),
+		addPreset(new TranslatableText("betternether.flat_nether"),
 				Blocks.NETHERRACK,
 				Biomes.NETHER_WASTES,
 				Arrays.asList("decoration", "nether_city"),
