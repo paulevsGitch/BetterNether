f6db0f3d5756aad2e0bc4ef1a5707fbbade7b5bd
paulevsGitch <paulevs@yandex.ru>
2020-04-30 20:34:37 +0300

    Mushrooms felling

--- src/main/java/paulevs/betternether/blocks/BlockGiantMold.java
+++ src/main/java/paulevs/betternether/blocks/BlockGiantMold.java
@@ -4,14 +4,17 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.MaterialColor;
 import net.minecraft.entity.EntityContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.IWorld;
 import paulevs.betternether.blocks.materials.Materials;
 import paulevs.betternether.blocks.shapes.TripleShape;
 import paulevs.betternether.registry.BlocksRegistry;
@@ -47,5 +50,19 @@ public class BlockGiantMold extends BlockBaseNotFull
 	{
 		TripleShape shape = state.get(SHAPE);
 		return shape == TripleShape.TOP ? new ItemStack(BlocksRegistry.GIANT_MOLD_SAPLING) : new ItemStack(BlocksRegistry.MUSHROOM_STEM);
+	}
+	
+	@Override
+	public BlockState getStateForNeighborUpdate(BlockState state, Direction facing, BlockState neighborState, IWorld world, BlockPos pos, BlockPos neighborPos)
+	{
+		switch (state.get(SHAPE))
+		{
+		case BOTTOM:
+			return state;
+		case MIDDLE:
+		case TOP:
+		default:
+			return world.getBlockState(pos.down()).getBlock() == this ? state : Blocks.AIR.getDefaultState();
+		}
 	}
 }
