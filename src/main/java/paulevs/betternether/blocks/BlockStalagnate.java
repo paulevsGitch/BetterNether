8042c2f35baba8deb201c629fe026bbb3818470b
paulevsGitch <paulevs@yandex.ru>
2020-04-30 08:24:52 +0300

    Stalagnate item

--- src/main/java/paulevs/betternether/blocks/BlockStalagnate.java
+++ src/main/java/paulevs/betternether/blocks/BlockStalagnate.java
@@ -1,10 +1,16 @@
 package paulevs.betternether.blocks;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.EnumProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import paulevs.betternether.blocks.shapes.TripleShape;
+import paulevs.betternether.registry.BlocksRegistry;
 
 public class BlockStalagnate extends BlockStalagnateStem
 {
@@ -13,7 +19,6 @@ public class BlockStalagnate extends BlockStalagnateStem
 	public BlockStalagnate()
 	{
 		super();
-		//this.setClimmable(true);
 		this.setRenderLayer(BNRenderLayer.CUTOUT);
 		this.setDefaultState(getStateManager().getDefaultState().with(SHAPE, TripleShape.MIDDLE));
 		this.setDropItself(false);
@@ -24,4 +29,11 @@ public class BlockStalagnate extends BlockStalagnateStem
 	{
         stateManager.add(SHAPE);
     }
+	
+	@Override
+	@Environment(EnvType.CLIENT)
+	public ItemStack getPickStack(BlockView world, BlockPos pos, BlockState state)
+	{
+		return new ItemStack(BlocksRegistry.STALAGNATE_STEM);
+	}
 }
