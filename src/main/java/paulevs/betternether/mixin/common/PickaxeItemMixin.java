20effc0936548c0be6d037360dc77afd7e205139
paulevsGitch <paulevs@yandex.ru>
2020-07-07 19:58:22 +0300

    Villagers and doors fix

--- src/main/java/paulevs/betternether/mixin/common/PickaxeItemMixin.java
+++ src/main/java/paulevs/betternether/mixin/common/PickaxeItemMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.MiningToolItem;
