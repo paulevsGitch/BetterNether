df20d694c9216f33c9a2e67b92a0beb0e672c96e
paulevsGitch <paulevs@yandex.ru>
2020-09-20 03:25:30 +0300

    Medicines, cactus growing

--- src/main/java/paulevs/betternether/blocks/BlockBarrelCactus.java
+++ src/main/java/paulevs/betternether/blocks/BlockBarrelCactus.java
@@ -1,27 +1,41 @@
 package paulevs.betternether.blocks;
 
-import java.util.Random;
+import java.util.List;
+
+import com.google.common.collect.Lists;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Fertilizable;
 import net.minecraft.block.Material;
 import net.minecraft.block.MaterialColor;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.loot.context.LootContext;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
+import paulevs.betternether.MHelper;
 
-public class BlockBarrelCactus extends BlockCactusBase implements Fertilizable
+public class BlockBarrelCactus extends BlockCommonPlant implements Fertilizable
 {
-	private static final VoxelShape SELECTION = Block.createCuboidShape(0, 0, 0, 16, 14, 16);
-	private static final VoxelShape COLLISION = Block.createCuboidShape(1, 0, 1, 15, 13, 15);
+	private static final VoxelShape[] SHAPES = new VoxelShape[] {
+			Block.createCuboidShape(5, 0, 5, 11, 5, 11),
+			Block.createCuboidShape(3, 0, 3, 13, 9, 13),
+			Block.createCuboidShape(2, 0, 2, 14, 12, 14),
+			Block.createCuboidShape(1, 0, 1, 15, 14, 15)
+	};
 	
 	public BlockBarrelCactus()
 	{
@@ -29,21 +43,45 @@ public class BlockBarrelCactus extends BlockCactusBase implements Fertilizable
 				.materialColor(MaterialColor.BLUE_TERRACOTTA)
 				.sounds(BlockSoundGroup.WOOL)
 				.nonOpaque()
-				.hardness(0.4F));
+				.hardness(0.4F)
+				.ticksRandomly()
+				.breakByTool(FabricToolTags.SHEARS));
+	}
+	
+	@Override
+	public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos)
+	{
+		Block down = world.getBlockState(pos.down()).getBlock();
+		return down == Blocks.GRAVEL;
+	}
+	
+	@Override
+	public BlockState getStateForNeighborUpdate(BlockState state, Direction facing, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos)
+	{
+		if (canPlaceAt(state, world, pos))
+			return state;
+		else
+			return Blocks.AIR.getDefaultState();
+	}
+
+	@Override
+	public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity)
+	{
+		if (state.get(BlockCommonPlant.AGE) > 1) entity.damage(DamageSource.CACTUS, 1.0F);
 	}
 	
 	@Override
 	public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext ePos)
 	{
 		Vec3d vec3d = state.getModelOffset(view, pos);
-		return SELECTION.offset(vec3d.x, vec3d.y, vec3d.z);
+		return SHAPES[state.get(BlockCommonPlant.AGE)].offset(vec3d.x, vec3d.y, vec3d.z);
 	}
 	
 	@Override
 	public VoxelShape getCollisionShape(BlockState state, BlockView view, BlockPos pos, ShapeContext ePos)
 	{
 		Vec3d vec3d = state.getModelOffset(view, pos);
-		return COLLISION.offset(vec3d.x, vec3d.y, vec3d.z);
+		return SHAPES[state.get(BlockCommonPlant.AGE)].offset(vec3d.x, vec3d.y, vec3d.z);
 	}
 	
 	@Override
@@ -53,20 +91,12 @@ public class BlockBarrelCactus extends BlockCactusBase implements Fertilizable
 	}
 	
 	@Override
-	public boolean isFertilizable(BlockView world, BlockPos pos, BlockState state, boolean isClient)
+	public List<ItemStack> getDroppedStacks(BlockState state, LootContext.Builder builder)
 	{
-		return true;
-	}
-
-	@Override
-	public boolean canGrow(World world, Random random, BlockPos pos, BlockState state)
-	{
-		return true;
-	}
-
-	@Override
-	public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state)
-	{
-		dropStack(world, pos, new ItemStack(this.asItem()));
+		if (state.get(BlockCommonPlant.AGE) == 3)
+		{
+			return Lists.newArrayList(new ItemStack(this, MHelper.randRange(1, 3, MHelper.RANDOM)));
+		}
+		return Lists.newArrayList(new ItemStack(this));
 	}
 }
--- src/main/java/paulevs/betternether/structures/plants/StructureBarrelCactus.java
+++ src/main/java/paulevs/betternether/structures/plants/StructureBarrelCactus.java
@@ -1,11 +1,12 @@
 package paulevs.betternether.structures.plants;
 
+import paulevs.betternether.blocks.BlockCommonPlant;
 import paulevs.betternether.registry.BlocksRegistry;
 
 public class StructureBarrelCactus extends StructureScatter
 {
 	public StructureBarrelCactus()
 	{
-		super(BlocksRegistry.BARREL_CACTUS);
+		super(BlocksRegistry.BARREL_CACTUS, BlockCommonPlant.AGE, 4);
 	}
 }
