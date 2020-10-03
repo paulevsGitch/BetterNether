b8948181a2cbdeb5002d6a4b6837a3484d3d804e
paulevsGitch <paulevs@yandex.ru>
2020-10-03 07:44:40 +0300

    Locate command fix

--- src/main/java/paulevs/betternether/world/CityHelper.java
+++ src/main/java/paulevs/betternether/world/CityHelper.java
@@ -1,8 +1,11 @@
 package paulevs.betternether.world;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.gen.ChunkRandom;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
@@ -48,5 +51,40 @@ public class CityHelper
 				return true;
 		}
 		return false;
+	}
+	
+	private static long sqr(int x)
+	{
+		return (long) x * (long) x;
+	}
+	
+	public static BlockPos getNearestCity(BlockPos pos, ServerWorld world)
+	{
+		int cx = pos.getX() >> 4;
+		int cz = pos.getZ() >> 4;
+		
+		StructureConfig config = world.getChunkManager().getChunkGenerator().getStructuresConfig().getForType(BNWorldGenerator.CITY);
+		if (config == null)
+			return null;
+		
+		collectNearby(cx, cz, config, world.getSeed(), new ChunkRandom());
+		Iterator<ChunkPos> iterator = POSITIONS.iterator();
+		if (iterator.hasNext())
+		{
+			ChunkPos nearest = POSITIONS.iterator().next();
+			long d = sqr(nearest.x - cx) + sqr(nearest.z - cz);
+			while (iterator.hasNext())
+			{
+				ChunkPos n = iterator.next();
+				long d2 = sqr(n.x - cx) + sqr(n.z - cz);
+				if (d2 < d)
+				{
+					d = d2;
+					nearest = n;
+				}
+			}
+			return nearest.getStartPos();
+		}
+		return null;
 	}
 }
