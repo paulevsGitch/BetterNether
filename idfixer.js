4595033699e913606c56c6c024cfeeaf125b01b9
Frank <developer@ambertation.de>
2021-07-22 23:43:05 +0200

    all id patches

--- idfixer.js
+++ idfixer.js
@@ -11,9 +11,26 @@ const Map = {
 }
 
 const replacments = [
+    Map.entry("betternether:striped_log_stalagnate", "betternether:stalagnate_stripped_log"),
+    Map.entry("betternether:striped_bark_stalagnate", "betternether:stalagnate_stripped_bark"),
+    Map.entry("betternether:stalagnate_planks_stairs", "betternether:stalagnate_stairs"),
+    Map.entry("betternether:stalagnate_planks_slab", "betternether:stalagnate_slab"),
+    Map.entry("betternether:stalagnate_planks_fence", "betternether:stalagnate_fence"),
+    Map.entry("betternether:stalagnate_planks_gate", "betternether:stalagnate_gate"),
+    Map.entry("betternether:stalagnate_planks_button", "betternether:stalagnate_button"),
+    Map.entry("betternether:stalagnate_planks_plate", "betternether:stalagnate_plate"),
+    Map.entry("betternether:stalagnate_planks_trapdoor", "betternether:stalagnate_trapdoor"),
+    Map.entry("betternether:stalagnate_planks_door", "betternether:stalagnate_door"),
+    Map.entry("betternether:crafting_table_stalagnate", "betternether:stalagnate_crafting_table"),
+    Map.entry("betternether:sign_stalagnate", "betternether:stalagnate_sign"),
+    Map.entry("betternether:chest_stalagnate", "betternether:stalagnate_chest"),
+    Map.entry("betternether:barrel_stalagnate", "betternether:stalagnate_barrel"),
+    Map.entry("betternether:roof_tile_stalagnate", "betternether:stalagnate_roof"),
+    Map.entry("betternether:roof_tile_stalagnate_stairs", "betternether:stalagnate_roof_stairs"),
+    Map.entry("betternether:roof_tile_stalagnate_slab", "betternether:stalagnate_roof_slab"),
     Map.entry("betternether:taburet_stalagnate", "betternether:stalagnate_taburet"),
-				Map.entry("betternether:chair_stalagnate", "betternether:stalagnate_chair"),
-				Map.entry("betternether:bar_stool_stalagnate", "betternether:stalagnate_bar_stool")
+    Map.entry("betternether:chair_stalagnate", "betternether:stalagnate_chair"),
+    Map.entry("betternether:bar_stool_stalagnate", "betternether:stalagnate_bar_stool")
         ]
 
 function findFiles(dir, indent=""){
