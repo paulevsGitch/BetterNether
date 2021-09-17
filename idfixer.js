2a88651c77b19b0957caca0537d7cf8c599bc7e3
Frank <developer@ambertation.de>
2021-09-17 10:27:16 +0200

    Fixer for AnchorTree

--- idfixer.js
+++ idfixer.js
@@ -10,20 +10,18 @@ const Map = {
     }
 }
 
-const replacments = [
-    Map.entry("betternether:striped_log_wart", "betternether:wart_stripped_log"),
-				Map.entry("betternether:striped_bark_wart", "betternether:wart_stripped_bark"),
-				Map.entry("betternether:crafting_table_wart", "betternether:wart_crafting_table"),
-				Map.entry("betternether:sign_wart", "betternether:wart_sign"),
-				Map.entry("betternether:chest_wart", "betternether:wart_chest"),
-				Map.entry("betternether:barrel_wart", "betternether:wart_barrel"),
-				Map.entry("betternether:roof_tile_wart", "betternether:wart_roof"),
-				Map.entry("betternether:roof_tile_wart_stairs", "betternether:wart_roof_stairs"),
-				Map.entry("betternether:roof_tile_wart_slab", "betternether:wart_roof_slab"),
-				Map.entry("betternether:taburet_wart", "betternether:wart_taburet"),
-				Map.entry("betternether:chair_wart", "betternether:wart_chair"),
-				Map.entry("betternether:bar_stool_wart", "betternether:wart_bar_stool")
-        ]
+const replacments = [  
+    Map.entry("betternether:striped_log_anchor_tree", "betternether:anchor_tree_stripped_log"),
+    Map.entry("betternether:striped_bark_anchor_tree", "betternether:anchor_tree_stripped_bark"),
+    Map.entry("betternether:crafting_table_anchor_tree", "betternether:anchor_tree_crafting_table"),
+    Map.entry("betternether:sign_anchor_tree", "betternether:anchor_tree_sign"),
+    Map.entry("betternether:chest_anchor_tree", "betternether:anchor_tree_chest"),
+    Map.entry("betternether:barrel_anchor_tree", "betternether:anchor_tree_barrel"),
+    Map.entry("betternether:taburet_anchor_tree", "betternether:anchor_tree_taburet"),
+    Map.entry("betternether:chair_anchor_tree", "betternether:anchor_tree_chair"),
+    Map.entry("betternether:bar_stool_anchor_tree", "betternether:anchor_tree_bar_stool") 
+        
+]
 
 function findFiles(dir, indent=""){
     console.log(`${indent}|-- ${dir}`)
@@ -45,7 +43,7 @@ function findFiles(dir, indent=""){
                         // const search = what.old  
                         // const replacer = new RegExp(search, 'g')
                         // jsonnew = jsonnew.replace(replacer, what.new)
-                        jsonnew = jsonnew.split(what.old).join( what.new)
+                        jsonnew = jsonnew.split(what.old).join(what.new)
                         
                     })
                     if (json!=jsonnew){
