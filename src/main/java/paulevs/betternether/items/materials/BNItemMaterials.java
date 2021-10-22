dcd4eba75f5ee79aa02e5cd286cbab4914a40586
Mandalorian007 <mandalorian007@gmail.com>
2021-10-22 09:07:17 -0400

    Adjustments to the durability of better nether tools to keep them more balanced with vanilla and better end

--- src/main/java/paulevs/betternether/items/materials/BNItemMaterials.java
+++ src/main/java/paulevs/betternether/items/materials/BNItemMaterials.java
@@ -10,8 +10,8 @@ public class BNItemMaterials {
 	public static final ArmorMaterial CINCINNASITE_ARMOR = new BNArmorMaterial("cincinnasite", 12, 4, SoundEvents.ARMOR_EQUIP_IRON, NetherItems.CINCINNASITE_INGOT, 1F, new int[] { 3, 5, 7, 3 });
 	public static final ArmorMaterial NETHER_RUBY_ARMOR = new BNArmorMaterial("nether_ruby", 13, 3, SoundEvents.ARMOR_EQUIP_IRON, NetherItems.NETHER_RUBY, 1F, new int[] { 3, 5, 7, 3 });
 
-	public static final Tier CINCINNASITE_TOOLS = new BNToolMaterial(512, 6F, 2, 14, 2.5F, NetherItems.CINCINNASITE_INGOT);
-	public static final Tier CINCINNASITE_DIAMOND_TOOLS = new BNToolMaterial(1800, 8F, 3, 22, 3.5F, Items.DIAMOND);
+	public static final Tier CINCINNASITE_TOOLS = new BNToolMaterial(300, 6F, 2, 14, 2.5F, NetherItems.CINCINNASITE_INGOT);
+	public static final Tier CINCINNASITE_DIAMOND_TOOLS = new BNToolMaterial(1650, 8F, 3, 22, 3.5F, Items.DIAMOND);
 
-	public static final Tier NETHER_RUBY_TOOLS = new BNToolMaterial(1300, 4F, 3, 9, 2.0F, NetherItems.NETHER_RUBY);
+	public static final Tier NETHER_RUBY_TOOLS = new BNToolMaterial(750, 4F, 3, 9, 2.0F, NetherItems.NETHER_RUBY);
 }
