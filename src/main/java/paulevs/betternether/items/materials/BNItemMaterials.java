867cbecd60df813d4865628f9079e87def8ed328
Mandalorian007 <mandalorian007@gmail.com>
2021-10-26 22:39:55 -0400

    re-balance cinn-diamond tool enchantability to be closer to that of terminite and diamond rather than gold

--- src/main/java/paulevs/betternether/items/materials/BNItemMaterials.java
+++ src/main/java/paulevs/betternether/items/materials/BNItemMaterials.java
@@ -11,7 +11,7 @@ public class BNItemMaterials {
 	public static final ArmorMaterial NETHER_RUBY_ARMOR = new BNArmorMaterial("nether_ruby", 13, 3, SoundEvents.ARMOR_EQUIP_IRON, NetherItems.NETHER_RUBY, 1F, new int[] { 3, 5, 7, 3 });
 
 	public static final Tier CINCINNASITE_TOOLS = new BNToolMaterial(300, 6F, 2, 14, 2.5F, NetherItems.CINCINNASITE_INGOT);
-	public static final Tier CINCINNASITE_DIAMOND_TOOLS = new BNToolMaterial(1650, 8F, 3, 22, 3.5F, Items.DIAMOND);
+	public static final Tier CINCINNASITE_DIAMOND_TOOLS = new BNToolMaterial(1650, 8F, 3, 12, 3.5F, Items.DIAMOND);
 
 	public static final Tier NETHER_RUBY_TOOLS = new BNToolMaterial(750, 4F, 3, 9, 2.0F, NetherItems.NETHER_RUBY);
 }
