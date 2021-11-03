e099735be8658f4ba80c3432536309df9062240f
Frank <developer@ambertation.de>
2021-11-03 12:53:50 +0100

    `NetherPickaxe` performs better on all Obsidian Blocks

--- src/main/java/paulevs/betternether/items/NetherPickaxe.java
+++ src/main/java/paulevs/betternether/items/NetherPickaxe.java
@@ -4,6 +4,8 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import paulevs.betternether.BetterNether;
+import paulevs.betternether.registry.NetherBlocks;
 import paulevs.betternether.registry.NetherItems;
 import paulevs.betternether.registry.NetherTags;
 import ru.bclib.api.TagAPI;
@@ -16,8 +18,14 @@ public class NetherPickaxe extends BasePickaxeItem {
 
 	@Override
 	public float getDestroySpeed(ItemStack stack, BlockState state) {
-		if ((state.is(TagAPI.BLOCK_NETHER_STONES) || state.is(TagAPI.BLOCK_NETHER_PORTAL_FRAME)) && this.getTier().getLevel() > 2) {
-			return this.speed * 3;
+		if ((state.is(TagAPI.BLOCK_NETHER_STONES)
+			|| state.is(TagAPI.BLOCK_NETHER_PORTAL_FRAME)
+			|| state.is(Blocks.OBSIDIAN)
+			|| state.is(Blocks.CRYING_OBSIDIAN)
+			|| state.is(NetherBlocks.BLUE_CRYING_OBSIDIAN)
+			|| state.is(NetherBlocks.WEEPING_OBSIDIAN)
+			|| state.is(NetherBlocks.BLUE_WEEPING_OBSIDIAN)) && this.getTier().getLevel() > 2) {
+			return super.getDestroySpeed(stack, state) * 10;
 		}
 		return super.getDestroySpeed(stack, state);
 	}
