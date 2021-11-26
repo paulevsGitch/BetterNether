0065a9fcc869cb3fcb83d453746ba50843b931b1
Frank <developer@ambertation.de>
2021-11-26 16:23:55 +0100

    Use new BCLib `SpawnAPI`(#438)

--- src/main/java/paulevs/betternether/mixin/common/SpawnRestrictionMixin.java
+++ src/main/java/paulevs/betternether/mixin/common/SpawnRestrictionMixin.java
@@ -6,12 +6,14 @@ import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.level.levelgen.Heightmap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import paulevs.betternether.BlocksHelper;
 import paulevs.betternether.entity.EntityFirefly;
 import paulevs.betternether.entity.EntityHydrogenJellyfish;
 import paulevs.betternether.entity.EntityJungleSkeleton;
 import paulevs.betternether.entity.EntityNaga;
 import paulevs.betternether.entity.EntitySkull;
 import paulevs.betternether.registry.EntityRegistry;
+import ru.bclib.api.SpawnAPI;
 
 @Mixin(SpawnPlacements.class)
 public class SpawnRestrictionMixin {
@@ -19,10 +21,10 @@ public class SpawnRestrictionMixin {
 	private static <T extends Mob> void register(EntityType<T> type, SpawnPlacements.Type location, Heightmap.Types heightmapType, SpawnPlacements.SpawnPredicate<T> predicate) {}
 
 	static {
-		register(EntityRegistry.NAGA, SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, EntityNaga::canSpawn);
-		register(EntityRegistry.FIREFLY, SpawnPlacements.Type.NO_RESTRICTIONS, Heightmap.Types.MOTION_BLOCKING, EntityFirefly::canSpawn);
-		register(EntityRegistry.HYDROGEN_JELLYFISH, SpawnPlacements.Type.NO_RESTRICTIONS, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, EntityHydrogenJellyfish::canSpawn);
-		register(EntityRegistry.JUNGLE_SKELETON, SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, EntityJungleSkeleton::canSpawn);
-		register(EntityRegistry.SKULL, SpawnPlacements.Type.NO_RESTRICTIONS, Heightmap.Types.MOTION_BLOCKING, EntitySkull::canSpawn);
+		register(EntityRegistry.NAGA, SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, new SpawnAPI<>(EntityNaga.class).notPeacefulBelowBrightness(8).maxAlive()::canSpawn);
+		register(EntityRegistry.FIREFLY, SpawnPlacements.Type.NO_RESTRICTIONS, Heightmap.Types.MOTION_BLOCKING, new SpawnAPI<>(EntityFirefly.class).notAboveBlock(BlocksHelper::isLava, 7).maxAlive(8)::canSpawn);
+		register(EntityRegistry.HYDROGEN_JELLYFISH, SpawnPlacements.Type.NO_RESTRICTIONS, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, new SpawnAPI<>(EntityHydrogenJellyfish.class).maxAlive()::canSpawn);
+		register(EntityRegistry.JUNGLE_SKELETON, SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, new SpawnAPI<>(EntityJungleSkeleton.class).notPeaceful()::canSpawn);
+		register(EntityRegistry.SKULL, SpawnPlacements.Type.NO_RESTRICTIONS, Heightmap.Types.MOTION_BLOCKING, new SpawnAPI<>(EntitySkull.class).notPeacefulBelowBrightness().belowMinHeight().maxAlive()::canSpawn);
 	}
 }
