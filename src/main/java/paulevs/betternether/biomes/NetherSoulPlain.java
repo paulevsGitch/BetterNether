beadb1272050a8348354c66bdd90e2c1f73ca834
Frank <developer@ambertation.de>
2021-12-04 21:10:10 +0100

    Use SurfaceRules to distribute blocks in *SoulPlain*

--- src/main/java/paulevs/betternether/biomes/NetherSoulPlain.java
+++ src/main/java/paulevs/betternether/biomes/NetherSoulPlain.java
@@ -11,9 +11,9 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Blocks;
-import paulevs.betternether.BlocksHelper;
-import paulevs.betternether.MHelper;
-import paulevs.betternether.blocks.BlockSoulSandstone;
+import net.minecraft.world.level.levelgen.Noises;
+import net.minecraft.world.level.levelgen.SurfaceRules;
+import net.minecraft.world.level.levelgen.placement.CaveSurface;
 import paulevs.betternether.noise.OpenSimplexNoise;
 import paulevs.betternether.registry.NetherBlocks;
 import paulevs.betternether.structures.StructureType;
@@ -21,9 +21,14 @@ import paulevs.betternether.structures.plants.StructureBlackBush;
 import paulevs.betternether.structures.plants.StructureSoulGrass;
 import paulevs.betternether.structures.plants.StructureSoulVein;
 import ru.bclib.api.biomes.BCLBiomeBuilder;
-import ru.bclib.world.biomes.BCLBiome;
 
 public class NetherSoulPlain extends NetherBiome {
+	private static final SurfaceRules.RuleSource SOUL_SAND = SurfaceRules.state(Blocks.SOUL_SAND.defaultBlockState());
+	private static final SurfaceRules.RuleSource SOUL_SOIL = SurfaceRules.state(Blocks.SOUL_SOIL.defaultBlockState());
+	private static final SurfaceRules.RuleSource SOUL_SANDSTONE = SurfaceRules.state(NetherBlocks.SOUL_SANDSTONE.defaultBlockState());
+	private static final SurfaceRules.ConditionSource NOISE_SOUL_LAYER = SurfaceRules.noiseCondition(Noises.SOUL_SAND_LAYER, -0.012);
+	private static final SurfaceRules.ConditionSource NOISE_NETHER_STATE = SurfaceRules.noiseCondition(Noises.NETHER_STATE_SELECTOR, 0.0);
+
 	public static class Config extends NetherBiomeConfig {
 		public Config(String name) {
 			super(name);
@@ -36,9 +41,18 @@ public class NetherSoulPlain extends NetherBiome {
 				   .additions(SoundEvents.AMBIENT_CRIMSON_FOREST_ADDITIONS)
 				   .mood(SoundEvents.AMBIENT_CRIMSON_FOREST_MOOD)
 				   .particles(ParticleTypes.PORTAL, 0.02F)
-				   .surface(Blocks.SOUL_SOIL)
+				   //.surface(Blocks.SOUL_SOIL)
+					.surface(
+						SurfaceRules.sequence(
+								SurfaceRules.ifTrue(SurfaceRules.ON_CEILING, SOUL_SANDSTONE),
+								SurfaceRules.ifTrue(SurfaceRules.stoneDepthCheck(3, true, false, CaveSurface.FLOOR),
+										SurfaceRules.sequence(SurfaceRules.ifTrue(NOISE_SOUL_LAYER, SOUL_SOIL), SOUL_SAND)
+								),
+								SurfaceRules.ifTrue(NOISE_NETHER_STATE, SOUL_SOIL), SOUL_SANDSTONE)
+						)
+
 				   .structure(NetherBiomeBuilder.VANILLA_STRUCTURES.getNETHER_FOSSILE())
-				   .structure(NetherBiomeBuilder.VANILLA_STRUCTURES.getBASTION_REMNANT());;
+				   .structure(NetherBiomeBuilder.VANILLA_STRUCTURES.getBASTION_REMNANT());
 		}
 		
 		@Override
@@ -60,27 +74,27 @@ public class NetherSoulPlain extends NetherBiome {
 
 	@Override
 	public void genSurfColumn(LevelAccessor world, BlockPos pos, Random random) {
-		POS.set(pos);
-		int d1 = MHelper.randRange(2, 4, random);
+//		POS.set(pos);
+//		int d1 = MHelper.randRange(2, 4, random);
 
-		for (int i = 0; i < d1; i++) {
-			POS.setY(pos.getY() - i);
-			if (BlocksHelper.isNetherGround(world.getBlockState(POS)))
-				if (TERRAIN.eval(pos.getX() * 0.1, pos.getY() * 0.1, pos.getZ() * 0.1) > 0)
-					BlocksHelper.setWithoutUpdate(world, POS, Blocks.SOUL_SOIL.defaultBlockState());
-				else
-				BlocksHelper.setWithoutUpdate(world, POS, Blocks.SOUL_SAND.defaultBlockState());
-			else
-				return;
-		}
+//		for (int i = 0; i < d1; i++) {
+//			POS.setY(pos.getY() - i);
+//			if (BlocksHelper.isNetherGround(world.getBlockState(POS)))
+//				if (TERRAIN.eval(pos.getX() * 0.1, pos.getY() * 0.1, pos.getZ() * 0.1) > 0)
+//					BlocksHelper.setWithoutUpdate(world, POS, Blocks.SOUL_SOIL.defaultBlockState());
+//				else
+//				BlocksHelper.setWithoutUpdate(world, POS, Blocks.SOUL_SAND.defaultBlockState());
+//			else
+//				return;
+//		}
 
-		int d2 = MHelper.randRange(5, 7, random);
-		for (int i = d1; i < d2; i++) {
-			POS.setY(pos.getY() - i);
-			if (BlocksHelper.isNetherGround(world.getBlockState(POS)))
-				BlocksHelper.setWithoutUpdate(world, POS, NetherBlocks.SOUL_SANDSTONE.defaultBlockState().setValue(BlockSoulSandstone.UP, i == d1));
-			else
-				return;
-		}
+//		int d2 = MHelper.randRange(5, 7, random);
+//		for (int i = d1; i < d2; i++) {
+//			POS.setY(pos.getY() - i);
+//			if (BlocksHelper.isNetherGround(world.getBlockState(POS)))
+//				BlocksHelper.setWithoutUpdate(world, POS, NetherBlocks.SOUL_SANDSTONE.defaultBlockState().setValue(BlockSoulSandstone.UP, i == d1));
+//			else
+//				return;
+//		}
 	}
 }
