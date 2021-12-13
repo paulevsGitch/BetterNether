f9208245f480c3b154c921acb9ad64156da2ece1
Frank <developer@ambertation.de>
2021-12-13 16:10:39 +0100

    Updated ModMenuIntegration

--- src/main/java/paulevs/betternether/config/ModMenuEntry.java
+++ src/main/java/paulevs/betternether/config/ModMenuEntry.java
@@ -1,12 +1,14 @@
 package paulevs.betternether.config;
 
 
-import com.terraformersmc.modmenu.util.ModMenuApiMarker;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import paulevs.betternether.config.screen.ConfigScreen;
 import ru.bclib.integration.ModMenuIntegration;
 
+import java.lang.reflect.Proxy;
+
 public class ModMenuEntry extends ModMenuIntegration {
-	public static final ModMenuApiMarker entrypointObject = createEntrypoint(new ModMenuEntry());
+	public static final ModMenuApi entrypointObject = createEntrypoint(new ModMenuEntry());
 
 	public ModMenuEntry() {
 		super(ConfigScreen::new);
