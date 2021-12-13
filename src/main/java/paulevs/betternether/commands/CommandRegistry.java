77188df9037e3cdf4f9c3c216c1023d032c77c45
Frank <developer@ambertation.de>
2021-12-13 18:14:47 +0100

    Clixe command to debug surface rules

--- src/main/java/paulevs/betternether/commands/CommandRegistry.java
+++ src/main/java/paulevs/betternether/commands/CommandRegistry.java
@@ -91,6 +91,14 @@ public class CommandRegistry {
                                   .then(Commands.argument("name", StringArgumentType.string())
                                     .executes(ctx -> placeNbt(ctx, StringArgumentType.getString(ctx, "name"))))
                     )
+                    .then(Commands.literal("sliceY")
+                                  .requires(source -> source.hasPermission(Commands.LEVEL_OWNERS) )
+                                  .executes(ctx -> slice(ctx, true))
+                    )
+                    .then(Commands.literal("sliceX")
+                                  .requires(source -> source.hasPermission(Commands.LEVEL_OWNERS) )
+                                  .executes(ctx -> slice(ctx, false))
+                    )
         );
     }
 
@@ -185,25 +193,23 @@ public class CommandRegistry {
     
     private static Map<Biome, BlockState> biomeMap = new HashMap<>();
     private static int biomeMapIdx = 0;
+    private static BlockState[] states = {
+        Blocks.RED_STAINED_GLASS.defaultBlockState(),
+        Blocks.BLUE_STAINED_GLASS.defaultBlockState(),
+        Blocks.YELLOW_STAINED_GLASS.defaultBlockState(),
+        Blocks.LIME_STAINED_GLASS.defaultBlockState(),
+        Blocks.PINK_STAINED_GLASS.defaultBlockState(),
+        Blocks.GREEN_STAINED_GLASS.defaultBlockState(),
+        Blocks.WHITE_STAINED_GLASS.defaultBlockState(),
+        Blocks.BLACK_STAINED_GLASS.defaultBlockState(),
+        Blocks.ORANGE_STAINED_GLASS.defaultBlockState()
+    };
     
     private static int revealOre(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
         final CommandSourceStack source = ctx.getSource();
         final ServerLevel level = source.getLevel();
-        final ServerPlayer player = source.getPlayerOrException();
         final Vec3 pos = source.getPosition();
-        final BlockPos blockPos = new BlockPos(pos);
-    
-        BlockState[] states = {
-            Blocks.RED_STAINED_GLASS.defaultBlockState(),
-            Blocks.BLUE_STAINED_GLASS.defaultBlockState(),
-            Blocks.YELLOW_STAINED_GLASS.defaultBlockState(),
-            Blocks.LIME_STAINED_GLASS.defaultBlockState(),
-            Blocks.PINK_STAINED_GLASS.defaultBlockState(),
-            Blocks.GREEN_STAINED_GLASS.defaultBlockState(),
-            Blocks.WHITE_STAINED_GLASS.defaultBlockState(),
-            Blocks.BLACK_STAINED_GLASS.defaultBlockState(),
-            Blocks.ORANGE_STAINED_GLASS.defaultBlockState()
-        };
+        
         MutableBlockPos bp = new MutableBlockPos();
         BlockState state;
         BlockState fillState;
@@ -232,6 +238,40 @@ public class CommandRegistry {
                             BlocksHelper.setWithoutUpdate(level, bp, fillState);
                         }
                     }
+                }
+            }
+        }
+        return Command.SINGLE_SUCCESS;
+    }
+    
+    private static int slice(CommandContext<CommandSourceStack> ctx, boolean constX) throws CommandSyntaxException {
+        final CommandSourceStack source = ctx.getSource();
+        final ServerLevel level = source.getLevel();
+        final Vec3 pos = source.getPosition();
+        
+        BlockState AIR = Blocks.AIR.defaultBlockState();
+        MutableBlockPos bp = new MutableBlockPos();
+        BlockState state;
+        BlockState fillState;
+        
+        
+        for (int y=1; y<127; y++){
+            bp.setY(y);
+            for (int x=constX?0:-64; x<64; x++){
+                bp.setX((int)pos.x+x);
+                for (int z=constX?-64:0; z<64; z++){
+                    bp.setZ((int)pos.z+z);
+                    if (y==1) {
+                        Biome b = level.getBiome(bp);
+                        fillState = biomeMap.computeIfAbsent(b, (bb)-> {
+                            biomeMapIdx = (biomeMapIdx + 1) % states.length;
+                            return states[biomeMapIdx];
+                        });
+                    } else {
+                        fillState = AIR;
+                    }
+                    
+                   BlocksHelper.setWithoutUpdate(level, bp, fillState);
                 }
             }
         }
