1eefe1ac7806ca7bf88c58ddc539c249984f878e
Frank <developer@ambertation.de>
2021-12-20 00:14:33 +0100

    Added interface

--- src/main/java/paulevs/betternether/blocks/BNWoodlikeDoor.java
+++ src/main/java/paulevs/betternether/blocks/BNWoodlikeDoor.java
@@ -6,10 +6,11 @@ import net.minecraft.tags.Tag.Named;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import ru.bclib.blocks.BaseDoorBlock;
+import ru.bclib.interfaces.TagProvider;
 
 import java.util.List;
 
-public class BNWoodlikeDoor extends BaseDoorBlock {
+public class BNWoodlikeDoor extends BaseDoorBlock implements TagProvider {
 	
 	public BNWoodlikeDoor(Block source) {
 		super(source);
