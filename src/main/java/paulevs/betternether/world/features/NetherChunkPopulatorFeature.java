2dada16730c5b16f228333690b180444b0e48c06
Frank <developer@ambertation.de>
2022-01-28 17:43:35 +0100

    Using ThreadLocal storage

--- src/main/java/paulevs/betternether/world/features/NetherChunkPopulatorFeature.java
+++ src/main/java/paulevs/betternether/world/features/NetherChunkPopulatorFeature.java
@@ -6,36 +6,20 @@ import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 import paulevs.betternether.BetterNether;
 import paulevs.betternether.world.BNWorldGenerator;
-import ru.bclib.BCLib;
-import ru.bclib.registry.BaseRegistry;
 import ru.bclib.world.features.DefaultFeature;
 
-import java.util.HashMap;
-import java.util.Random;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class NetherChunkPopulatorFeature extends DefaultFeature {
-	private static final ConcurrentHashMap<Thread, BNWorldGenerator> generatorPool = new ConcurrentHashMap<>();
-	
-	public static void clearGeneratorPool(){
-		generatorPool.clear();
-	}
+	public static final ThreadLocal<BNWorldGenerator> WORLD_GENERATOR = ThreadLocal.withInitial(() -> new BNWorldGenerator());
+	public static void clearGeneratorPool(){}
 
-	public static BNWorldGenerator generatorForThread() { return generatorPool.computeIfAbsent(Thread.currentThread(), t-> new BNWorldGenerator()); }
+	public static BNWorldGenerator generatorForThread() { return WORLD_GENERATOR.get(); }
 	public static final AtomicInteger callCounter = new AtomicInteger();
 	@Override
 	public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> featurePlaceContext) {
-		if (generatorPool.size()>15){
-			if (callCounter.incrementAndGet()>5000){
-				callCounter.set(0);
-				long deadCount = generatorPool.keySet().stream().filter((thread)->!thread.isAlive()).count();
-				BetterNether.LOGGER.info("Cleanup GeneratorPool: " + deadCount + "/" + generatorPool.size() );
-				;
-				generatorPool.entrySet().removeIf(e -> !e.getKey().isAlive());
-			}
-		}
-
 		final BlockPos worldPos = featurePlaceContext.origin();
 		final WorldGenLevel level = featurePlaceContext.level();
 		final int sx = (worldPos.getX() >> 4) << 4;
