23fa33bbd2adaada912be36b0aafe76eaeef67fd
Frank <developer@ambertation.de>
2022-06-08 11:34:50 +0200

    BCLib Supported ParticleType handling

--- src/main/java/org/betterx/betternether/particles/BNSimpleParticleType.java
+++
@@ -1,7 +0,0 @@
-package org.betterx.betternether.particles;
-
-public class BNSimpleParticleType extends net.minecraft.core.particles.SimpleParticleType {
-    public BNSimpleParticleType(boolean bl) {
-        super(bl);
-    }
-}
