50b4e23b6aebd05287ff3a1cd3bcc2bac9aa8b2c
Frank <developer@ambertation.de>
2022-08-01 01:14:41 +0200

    Adopted changes to BCLib

--- src/main/java/org/betterx/betternether/registry/NetherPoiTypes.java
+++ src/main/java/org/betterx/betternether/registry/NetherPoiTypes.java
@@ -1,10 +1,11 @@
 package org.betterx.betternether.registry;
 
 import org.betterx.bclib.api.v2.poi.BCLPoiType;
+import org.betterx.bclib.api.v2.poi.PoiManager;
 import org.betterx.betternether.BetterNether;
 
 public class NetherPoiTypes {
-    public static final BCLPoiType PIG_STATUE = BCLPoiType.register(
+    public static final BCLPoiType PIG_STATUE = PoiManager.register(
             BetterNether.makeID("pig_statue"),
             BCLPoiType.getBlockStates(NetherBlocks.PIG_STATUE_RESPAWNER),
             1, 1
